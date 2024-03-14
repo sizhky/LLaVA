4c715c7f8779d9e73a0775788a8c8d178004a5eb
injeti.mohan <injeti.mohan@ril.com>
2024-03-14 09:43:58 +0000

    add acausal model

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -2,5 +2,6 @@ try:
     from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
     from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
     from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
-except:
-    pass
+    from .language_model.llava_mistral_acausal import LlavaMistralForAcausalLM
+except Exception as e:
+    print(f'Exception: {e} during `from llava.model import *`')
